29bac3bdb8302e9e80ae2affee26e1a4e601d23a
DukeEnglish <ljyduke@gmail.com>
2020-01-24 18:14:13 +0800

    init

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 This is a basic translation system implemented from zero on a train to my home.
 
-1.20
 
-I did this for get familiar with the basic implementation of Python and take NMT as an example.
-
-
-# nmt_pytorch
---
+++ model/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#-*- coding:UTF-8 -*-
+#-----------------------------------------------------------------------#
+# File Name: __init__
+# Author: Junyi Li
+# Mail: 4ljy@163.com
+# Created Time: 2020-01-23
+# Description: 
+#-----------------------------------------------------------------------#
+
+import sys
+import os
+sys.path.append('../../../')
+sys.path.append('../../')
+sys.path.append('../')
